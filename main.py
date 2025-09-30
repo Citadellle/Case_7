4c7a26a7768823c1cb33416ffa464d6bb8851683
Mikhail Gordeev <miha_gord@mail.ru>
2025-09-30 22:30:55 +0700

    Updating old functions and downloading new functions.py

--- main.py
+++ main.py
@@ -1,29 +1,116 @@
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+from tqdm import tqdm
 from ru_local import *
 from const import *
 
 
-def link():
+def search_urls_pages():
     """
     creates a list containing links to all the necessary product pages
+    use libraries, like 'requests', 'bs4', 'const'
 
     :return url_list:
     """
-    
-    link4 = input()
-
-    url = fr'{LINK1}0{LINK3}{link4}'
-    r = requests.get(url)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    soup = soup.find('li', class_='page-num page_last').text
+    link4 = input('Введите запрос: ')
 
     url_list = []
-    for link2 in range(int(soup)):
-        url = fr'{LINK1}{link2}{LINK3}{link4}'
+    url = LINK1+'0'+LINK3+link4
+
+    r = requests.get(url)   
+    soup = BeautifulSoup(r.text, 'html.parser')
+    max_num_page = int(soup.find(class_='page-num page_last').text)
+
+    for link2 in range(max_num_page):
+        url = LINK1+str(link2)+LINK3+link4
         url_list.append(url)
 
     return url_list
+
+
+def search_urls_goods(url):
+    '''
+    function for searching all way's to goods in all page
+    use libraries, like 'requests', 'bs4'
+
+    :param only url:
+    :return urls list:
+    '''
+
+    #creating list for collecting ways to url page's
+    urls = []
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+
+    info = soup.find_all(class_='gr-product-name')
+    for i in info:
+        urls.append(i.find('a').get('href'))
+    
+    return urls
+
+
+
+def product_data(url):
+    '''
+    function for searching information of product
+    use libraries, like 'requests', 'bs4'
+
+    :param only url:
+    :return dict:
+    '''
+    page = requests.get(url)
+    bs = BeautifulSoup(page.text, 'html.parser')
+
+
+    keys = ['артикул', 'наименование', 'вид обуви',
+            'сезон', 'цена', 'размеры', 'материал верха',
+            'цвет', 'страна производитель']
+    data = dict.fromkeys(keys, '')
+    
+
+    data['артикул'] = bs.find(class_='shop2-product-article').find('span').next_sibling.strip()
+
+    data['наименование'] = bs.find(class_='gr-product-name').text.strip()
+
+    try:
+        data['вид обуви'] = bs.find(class_='shop2-product-params') \
+            .find_all(class_='param-item even')[4] \
+            .find(class_='param-body').text.strip()
+    except:
+        data['вид обуви'] = bs.find(class_='shop2-product-params') \
+            .find_all(class_='param-item odd')[4] \
+            .find(class_='param-body').text.strip()
+
+    param_sezon = bs.find(class_='option-item sezon odd')
+    if param_sezon is not None:
+        data['сезон'] = param_sezon.find(class_='option-body').text.strip()
+    else:
+        data['сезон'] = bs.find(class_='option-item sezon even') \
+            .find(class_='option-body').text.strip()
+    
+    data['цена'] = bs.find(class_='product-price') \
+        .find(class_='price-current').text.replace('\n', ' ').strip()
+
+    param_size = bs.find(class_='option-item razmery_v_korobke even')
+    if param_size is not None:
+        data['размеры'] =  param_size.find(class_='option-body').text.strip()
+    else:
+        data['размеры'] =  bs.find(class_='option-item razmery_v_korobke odd') \
+            .find(class_='option-body').text.strip()
+
+    data['материал верха'] = bs.find(class_='option-item material_verha_960 odd') \
+        .find(class_='option-body').text.strip()
+
+    data['цвет'] = bs.find(class_='option-item cvet odd') \
+        .find(class_='option-body').text.strip()
+
+    param_country = bs.find(class_='gr-vendor-block')
+    if param_country is not None:
+        data['страна производитель'] = param_country.text.strip()
+    else:
+        pass
+
+    return data
 
 
 def output(goods_data):
@@ -46,3 +133,27 @@ def output(goods_data):
             catalog.write(f'{COLOR}: {product[COLOR]}\n')
             catalog.write(f'{UP_MATERIAL}: {product[UP_MATERIAL]}\n')
             catalog.write(f'{COUNTRY}: {product[COUNTRY]}\n')
+
+
+def main():
+    '''
+    main function
+    '''
+    urls_pages = search_urls_pages()
+
+    urls_goods = []
+    for url_page in tqdm(urls_pages):
+        for url_product in search_urls_goods(url_page):
+            urls_goods.append(url_product)
+    
+    goods_data = []
+    for url in tqdm(urls_goods):
+        print(LINK + url)
+        goods_data.append(product_data(LINK + url))
+    
+    for i in goods_data:
+        print(i)
+    
+
+if __name__ == '__main__':
+    main()
