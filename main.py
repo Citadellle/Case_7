955a3739a7192bb31d37fa7a3076faf039cab9ae
givanov3-ux <g.ivanov3@g.nsu.ru>
2025-09-30 21:08:54 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ def link():
     LINK3 = '&gr_smart_search=1&s[sort_by]=price%20asc&search_text='
     link4 = input()
 
-    url_list = []
     url = fr'{LINK1}0{LINK3}{link4}'
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
     soup = soup.find('li', class_='page-num page_last').text
+
+    url_list = []
     for link2 in range(int(soup)):
         url = fr'{LINK1}{link2}{LINK3}{link4}'
-        print(r.status_code)
         url_list.append(url)
 
     return url_list
