8f4398c4bb8e5a083147d7f80a8132ba8c6a56b6
givanov3-ux <g.ivanov3@g.nsu.ru>
2025-10-01 09:03:49 +0700

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def search_urls_pages():
     if soup.find(class_='page-num page_last') is not None:
         max_num_page = int(soup.find(class_='page-num page_last').text)
     else:
-        max_num_page = 0
+        max_num_page = 1
 
     for link2 in range(max_num_page):
         url = fr'{LINK1}{link2}{LINK3}{link4}'
